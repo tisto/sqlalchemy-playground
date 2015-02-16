ef97dbad0cb731c9559490f4d42995719fc04cce
Timo Stollenwerk <tisto@plone.org>
2015-02-16 14:46:03 +0100

    Change example to book/author.

--- association_proxy/basic_association.py
+++ association_proxy/basic_association.py
@@ -1,66 +1,76 @@
 """basic_association.py
 
 illustrate a many-to-many relationship between an
-"Order" and a collection of "Item" objects, associating a purchase price
-with each via an association object called "OrderItem"
+"Book" and a collection of "Author" objects, associating a rank
+with each via an association object called "BookAuthor"
 
 The association object pattern is a form of many-to-many which
 associates additional data with each association between parent/child.
 
-The example illustrates an "order", referencing a collection
-of "items", with a particular price paid associated with each "item".
+The example illustrates an "book", referencing a collection
+of "authors", with a particular rank paid associated with each "author".
 
 """
 
-from datetime import datetime
-
-from sqlalchemy import (create_engine, MetaData, Table, Column, Integer,
-    String, DateTime, Float, ForeignKey, and_)
-from sqlalchemy.orm import mapper, relationship, Session
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class Order(Base):
-    __tablename__ = 'order'
+class Book(Base):
+    __tablename__ = 'book'
 
-    order_id = Column(Integer, primary_key=True)
-    customer_name = Column(String(30), nullable=False)
-    order_date = Column(DateTime, nullable=False, default=datetime.now())
-    order_items = relationship("OrderItem", cascade="all, delete-orphan",
-                            backref='order')
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    book_authors = relationship(
+        "BookAuthor",
+        cascade="all, delete-orphan",
+        backref='book'
+    )
 
-    def __init__(self, customer_name):
-        self.customer_name = customer_name
+    def __init__(self, title):
+        self.title = title
 
 
-class Item(Base):
-    __tablename__ = 'item'
-    item_id = Column(Integer, primary_key=True)
-    description = Column(String(30), nullable=False)
-    price = Column(Float, nullable=False)
+class Author(Base):
+    __tablename__ = 'author'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
 
-    def __init__(self, description, price):
-        self.description = description
-        self.price = price
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
-        return 'Item(%r, %r)' % (
-                    self.description, self.price
-                )
+        return 'Author(%s)' % self.name
 
 
-class OrderItem(Base):
-    __tablename__ = 'orderitem'
-    order_id = Column(Integer, ForeignKey('order.order_id'), primary_key=True)
-    item_id = Column(Integer, ForeignKey('item.item_id'), primary_key=True)
-    price = Column(Float, nullable=False)
+class BookAuthor(Base):
+    __tablename__ = 'bookauthor'
 
-    def __init__(self, item, price=None):
-        self.item = item
-        self.price = price or item.price
-    item = relationship(Item, lazy='joined')
+    book_id = Column(
+        Integer,
+        ForeignKey('book.id'),
+        primary_key=True
+    )
+    author_id = Column(
+        Integer,
+        ForeignKey('author.id'),
+        primary_key=True
+    )
+    rank = Column(String)
+
+    def __init__(self, author, rank=None):
+        self.author = author
+        self.rank = rank
+
+    author = relationship(Author, lazy='joined')
 
 
 if __name__ == '__main__':
@@ -69,34 +79,30 @@ if __name__ == '__main__':
 
     session = Session(engine)
 
-    # create catalog
-    tshirt, mug, hat, crowbar = (
-        Item('SA T-Shirt', 10.99),
-        Item('SA Mug', 6.50),
-        Item('SA Hat', 8.99),
-        Item('MySQL Crowbar', 16.99)
-    )
-    session.add_all([tshirt, mug, hat, crowbar])
+    # create two authors
+    chomsky = Author('Noam Chomsky')
+    miller = Author('George A . Miller')
+    session.add_all([chomsky, miller])
     session.commit()
 
-    # create an order
-    order = Order('john smith')
-
-    # add three OrderItem associations to the Order and save
-    order.order_items.append(OrderItem(mug))
-    order.order_items.append(OrderItem(crowbar, 10.99))
-    order.order_items.append(OrderItem(hat))
-    session.add(order)
+    # create an book with a single author
+    book1 = Book('Syntactic Structures')
+    book1.book_authors.append(BookAuthor(chomsky))
+    session.add(book1)
     session.commit()
 
-    # query the order, print items
-    order = session.query(Order).filter_by(customer_name='john smith').one()
-    print([(order_item.item.description, order_item.price)
-           for order_item in order.order_items])
+    # create a book with two authors
+    book2 = Book('Finite State Languages')
+    book2.book_authors.append(BookAuthor(chomsky, 'primary'))
+    book2.book_authors.append(BookAuthor(miller, 'secondary'))
+    session.add(book2)
+    session.commit()
 
-    # print customers who bought 'MySQL Crowbar' on sale
-    q = session.query(Order).join('order_items', 'item')
-    q = q.filter(and_(Item.description == 'MySQL Crowbar',
-                      Item.price > OrderItem.price))
-
-    print([order.customer_name for order in q])
+    # query the book, print authors with their rank
+    books = session.query(Book).all()
+    for book in books:
+        print('%s:' % book.title)
+        print([
+            (book_author.author.name, book_author.rank)
+            for book_author in book.book_authors
+        ])
